a01b580ef1729abde9dbe766ebaa54aba8d792a6
Itenga <itenga@protonamil.com>
2018-02-07 22:33:12 +0300

    spin

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { INFO } = require('./constants');
 const ChatServer = require('./server');
 const port = process.argv[2] || 56150;
 const host = process.argv[3] || '127.0.0.1';
 
 const server = new ChatServer();
-console.log(server);
+
+server.listen(port, host, () => {
+  server.log.info(`listening on ${JSON.stringify(server.address())}`);
+});
