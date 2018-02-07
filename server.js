c9cfa326c02b1ab84ff4ba826d693b26342d48cb
Itenga <itenga@protonamil.com>
2018-02-07 20:26:12 +0300

    events within

--- server.js
+++ server.js
@@ -6,12 +6,53 @@ const net = require('net');
 
 class ChatServer {
   constructor(cl, opts = {}) {
+    inherit(this, net.createServer(cl, opts));
+    this.log = Logger();
     this.connections = new ConnectionManager();
     this.sessions = new SessionManager();
-    this.logger = Logger();
+    this.medium = new Medium(this);
 
-    inherit(this, this._server, net.createServer(cl, opts));
+    this._init();
   }
-};
+
+  _init() {
+    this._onConnection();
+    this._onError();
+
+  }
+
+  _onConnection() {
+    this.on('connection', (socket) => {
+      this.connections.add(socket);
+      this._configureSocket(socket);
+    });
+  }
+
+  _configureSocket(socket) {
+    socket.setTimeout(180000);
+
+    socket.on('timeout', () => {
+      event = { type: 'timeout', socket: socket.id };
+      this.log(JSON.stringify(event));
+
+      this.sessions.end(socket);
+      this.connections.end(socket);
+    });
+
+    socket.on('data', (chunk) => {
+      const data = chunk.toString();
+      this.medium.broadcast(socket, data);
+    });
+
+    socket.on('error', err => this.log.error(err.message));
+    socket.on('end', () => {
+      this.killSession(socket);
+    });
+  }
+
+  _onError() {
+    this.on('error', err => this.log.error(err.message));
+  }
+}
 
 module.exports = ChatServer;
