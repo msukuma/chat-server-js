5078d5cc622fe30e58b927b74a352b274751d845
Itenga <itenga@protonamil.com>
2018-02-08 16:35:00 +0300

    added timestamp generator

--- util.js
+++ util.js
@@ -1,5 +1,14 @@
-exports.inherit = function inherit(child, parent) {
+module.exports = {
+  inherit: inherit,
+  isoTimeStamp: isoTimeStamp,
+};
+
+function inherit(child, parent) {
   for (let key in parent) {
     if (!child.hasOwnProperty(key)) child[key] = parent[key];
   }
-};
+}
+
+function isoTimeStamp() {
+  return new Date().toISOString();
+}
