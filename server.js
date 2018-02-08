d280e78a55b1ecde984e5182a1ed870356957991
Itenga <itenga@protonamil.com>
2018-02-08 14:34:28 +0300

    server update

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 const {
+  MESSAGE,
   ERROR,
   TIMEOUT,
   END,
   DATA,
-  CONNECTION, } = require('./constants');
+  CONNECTION,
+  LISTENING,
+  CLOSE,
+  SESSION, } = require('./constants');
 const { inherit } = require('./util');
 const Logger = require('./logger');
-const ConnectionManager = require('./connections');
-const SessionManager = require('./sessions');
+const ConnectionsManager = require('./connections');
+const SessionsManager = require('./sessions');
+const MessagesManager = require('./messages');
 const net = require('net');
 
 class ChatServer {
-  constructor(cl, opts = {}) {
-    inherit(this, net.createServer(cl, opts));
+  constructor(cl, options = {}) {
+    inherit(this, net.createServer(cl, options));
+    this.id = options.id || Date.now();
     this.log = Logger();
-    this.connections = new ConnectionManager();
-    this.sessions = new SessionManager();
-    this.medium = new Medium(this);
+    this.connections = new ConnectionsManager(this.log);
+    this.sessions = new SessionsManager(this.log);
+    this.messages = new MessagesManager(this);
 
     this._init();
   }
 
   _init() {
+    this._onListening();
     this._onConnection();
     this._onError();
+    this._onClose();
+  }
 
+  _onListening() {
+    this.on(LISTENING, () => {
+      const info = this.address();
+      info.type = LISTENING;
+      this.log.info(info);
+    });
   }
 
   _onConnection() {
@@ -38,26 +53,56 @@ class ChatServer {
     socket.setTimeout(180000);
 
     socket.on(TIMEOUT, () => {
-      event = { type: TIMEOUT, socket: socket.id };
-      this.log(JSON.stringify(event));
+      this.log.timeout({ socketId: socket.id });
 
       this.sessions.end(socket);
       this.connections.end(socket);
     });
 
     socket.on(DATA, (chunk) => {
-      const data = chunk.toString();
-      this.medium.broadcast(socket, data);
+      const data = JSON.parse(chunk.toString());
+
+      if (data.type === SESSION) {
+        this.sessions.add(socket, data.userId);
+      } else if (data.type === MESSAGE) {
+        if (this.messages.valid(data)) {
+          this.messages.broadcast(socket, data);
+        } else {
+          this.messages.error(socket, 'InvalidFormat');
+        }
+      }
+
     });
 
-    socket.on(ERROR, err => this.log.error(err.message));
+    socket.on(ERROR, err => {
+      this.log.error({
+        source: 'socket',
+        socketId: socket.id,
+        message: err.message,
+      });
+    });
+
     socket.on(END, () => {
-      this.killSession(socket);
+      this.sessions.end(socket);
+      this.connections.end(socket);
     });
   }
 
   _onError() {
-    this.on(ERROR, err => this.log.error(err.message));
+    this.on(ERROR, err => {
+      this.log.error({
+        serverId: this.id,
+        content: err.message,
+      });
+    });
+  }
+
+  _onClose() {
+    this.on(CLOSE, () => {
+      const info = this.address();
+      info.type = CLOSE;
+      this.log.info(info);
+    });
   }
 }
 
