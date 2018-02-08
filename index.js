5224a7f81bb60eac52c49c114416f905064f8b5e
Itenga <itenga@protonamil.com>
2018-02-08 14:35:51 +0300

    cleaned index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { INFO } = require('./constants');
 const ChatServer = require('./server');
-const port = process.argv[2] || 56150;
-const host = process.argv[3] || '127.0.0.1';
+const {
+  PORT,
+  HOST, } = require('./constants');
+const port = process.argv[2] || PORT;
+const host = process.argv[3] || HOST;
 
 const server = new ChatServer();
 
-server.listen(port, host, () => {
-  server.log.info(`listening on ${JSON.stringify(server.address())}`);
-});
+server.listen(port, host);
